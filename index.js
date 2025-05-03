f6aeddc77772a4180be355377f39d89cdc1b0a05
Mudit Gandhi <mudit2752004@gmail.com>
2025-05-03 17:40:24 +0530

    first commit

--- index.js
+++ index.js
@@ -105,6 +105,5 @@ app.post('/api/recommend-gpus', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('✅ GPU Recommender API running on port 5000');
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
